5935740b29d9eeb3af9453b7ee0edf598e822edb
Kseniya Davletova <sometimes_here@mail.ru>
2023-08-01 12:54:17 +0400

    init commit

---
+++ src/main/java/com/example/ehealthcarepatient/EHealthCarePatientApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehealthcarepatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthCarePatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHealthCarePatientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ehealthcarepatient/EHealthCarePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehealthcarepatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealthCarePatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
